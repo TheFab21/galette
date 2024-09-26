76375e887bcd518c83eb36ca6b88e2ca0d5e2368
TheFab21 <121039071+TheFab21@users.noreply.github.com>
2024-09-26 12:51:58 +0200

    Update PdfMembersCardsAdaptative.php

--- galette/lib/Galette/IO/PdfMembersCardsAdaptative.php
+++ galette/lib/Galette/IO/PdfMembersCardsAdaptative.php
@@ -49,30 +49,32 @@ class PdfMembersCardsAdaptative extends PdfMembersCards
         parent::init();
 
         //maximum size for visible text. May vary with fonts.
-        $this->max_text_size = self::getWidth() - (int)round($this->wi / 3.5) - 2;
+         $this->max_text_size_full = self::getWidth();
         $this->year_font_size = (int)round(self::getWidth() / 7);
+
+        
 
         $print_logo = new PrintLogo();
 
-        // Set logo size to max 33% width  or max 40% height
-        $ratio = $print_logo->getWidth() / $print_logo->getHeight();
+        // Set logo size to max 33% width  or max 45% height
+        $this->ratio = $print_logo->getWidth() / $print_logo->getHeight();
 
-        if ($ratio <= 1) {
-            if ($print_logo->getHeight() > 0.40 * $this->he * 3.78) {
-                $this->hlogo = round(0.40 * $this->he);
+        if ($this->ratio <= 1) {
+            if ($print_logo->getHeight() > 0.45 * $this->he * 3.78) {
+                $this->hlogo = round(0.45 * $this->he,0,PHP_ROUND_HALF_DOWN);
             } else {
                 // Convert original pixels size to millimeters
                 $this->hlogo = $print_logo->getHeight() / 3.78;
             }
-            $this->wlogo = round($this->hlogo * $ratio);
+            $this->wlogo = round($this->hlogo *  $this->ratio);
         } else {
             if ($print_logo->getWidth() > 0.33 * $this->wi * 3.78) {
-                $this->wlogo = round(0.33 * $this->wi);
+                $this->wlogo = round(0.33 * $this->wi,0,PHP_ROUND_HALF_DOWN);
             } else {
                 // Convert original pixels size to millimeters
                 $this->wlogo = $print_logo->getWidth() / 3.78;
             }
-            $this->hlogo = round($this->wlogo / $ratio);
+            $this->hlogo = round($this->wlogo / $this->ratio);
         }
     }
 
@@ -106,6 +108,8 @@ class PdfMembersCardsAdaptative extends PdfMembersCards
             $y0 = $this->yorigin + $row * (round($this->he) + round($this->vspacing));
             // Logo X position
             $xl = round($x0 + $this->wi - $this->wlogo);
+            // Baneer max height
+		    $this->ban_max_he = 12;
             // Get data
             $email = '';
             switch ($this->preferences->pref_card_address) {
@@ -148,8 +152,13 @@ class PdfMembersCardsAdaptative extends PdfMembersCards
             $photofile = $photo->getPath();
 
             // Photo 100x130 and logo
-            $this->Image($photofile, $x0 + 1, $y0 + 1, round($this->wi / 3.5));
+            $this->wphoto =  round($this->wi / 3.5);
+			$this->hphoto =  $this->wphoto * 1.30;
+            $this->Image($photofile, $x0 + 1, $y0 + 1, $this->wphoto );
             $this->Image($this->logofile, $xl - 1, $y0 + 1, round($this->wlogo));
+            // Define max space for text
+			$this->max_text_size_top = $this->max_text_size_full - intval($this->wlogo + $this->wphoto);
+			$this->max_text_size_center = $this->max_text_size_full - intval($this->wphoto);
 
             // Color=#8C8C8C: Shadow of the year
             $this->SetTextColor(140);
@@ -173,32 +182,63 @@ class PdfMembersCardsAdaptative extends PdfMembersCards
             $this->SetTextColor($fcol['R'], $fcol['G'], $fcol['B']);
 
             $this->SetFontSize(8);
-            $ratio = $this->wlogo / $this->hlogo;
-            if (!empty($this->preferences->pref_show_id) || !empty($member->number)) {
+            $this->SetFont(self::FONT,'B');
+            $this->ratio = $this->wlogo / $this->hlogo;
+           if (!empty($this->preferences->pref_show_id) || !empty($member->number)) {
                 $member_id = (!empty($member->number)) ? $member->number : $member->id;
-                $xid = $x0 + $this->wi / 2 - $this->GetStringWidth(_T("Member") . ' n° : ' . $member_id, self::FONT, 'B', 8) / 2;
-                if ($ratio > 1) {
-                    $this->SetXY($xid, $y0 + $this->hlogo);
-                } else {
-                    $this->SetXY($xid, $y0 + 7) ;
-                }
-                $this->writeHTML('<strong>' . _T("Member") . ' n° : ' . $member_id . '  </strong>', false, false);
-            }
+				$this->adh_nbr = _T("Member") . ' n° : ' . $member_id ;
+               if ($this->hlogo + 1 > 7 ) {
+				$this->fixSize(
+                $this->adh_nbr,
+                $this->max_text_size_top - 3 ,
+                8,
+                'B'
+            	);
+					$this->SetFontSize(round($this->FontSizePt,0,PHP_ROUND_HALF_DOWN));
+					$xid = $x0 + $this->wi / 2 - $this->GetStringWidth($this->adh_nbr,$this->FontFamily, $this->FontStyle, $this->FontSizePt) / 2 + $this->wphoto / 2 - $this->wlogo / 2;
+					$this->SetXY($xid, $y0 + 8) ;
+						} else {
+						$this->fixSize(
+                		$this->adh_nbr,
+                		$this->max_text_size_center,
+                		10,
+                		'B'
+            			);
+					$xid = $x0 + $this->wi / 2 - $this->GetStringWidth($this->adh_nbr, $this->FontFamily, $this->FontStyle, $this->FontSizePt) / 2 + $this->wphoto / 2;
+					$this->SetXY($xid, $y0 + 8) ;
+               }
+				$this->SetFontSize(round($this->FontSizePt,0,PHP_ROUND_HALF_DOWN));
+				$this->writeHTML('<strong>' . $this->adh_nbr . '  </strong>', false, false);
+           }
+            
             $this->SetFontSize($this->year_font_size);
             $xan_cot = $xan_cot - 0.1;
             $this->SetXY($xan_cot, $y0 + 1 - 0.1);
             $this->writeHTML('<strong>' . $an_cot . '</strong>', false, false);
 
             // Abbrev: Adapt font size to text length
-            $this->fixSize(
+            if ( 13 < $this->hlogo + 1 ) {
+				            $this->fixSize(
                 $this->abrev,
-                $this->max_text_size,
+                $this->max_text_size_top - 4,
                 12,
                 'B'
             );
-            $xid = $x0 + $this->wi / 2 - $this->GetStringWidth($this->abrev, self::FONT, 'B', 12) / 2;
-            $this->SetXY($xid, $y0 + 12);
-            $this->writeHTML('<strong>' . $this->abrev . '</strong>', true, false);
+				
+				$this->SetFontSize(round($this->FontSizePt,0,PHP_ROUND_HALF_DOWN)) ;
+				$xid = $x0 + $this->wi / 2 - $this->GetStringWidth($this->abrev, $this->FontFamily, $this->FontStyle,$this->FontSizePt) / 2 + $this->wphoto/2 - $this->wlogo/2  ;
+			} else {
+				$this->fixSize(
+                $this->abrev,
+                $this->max_text_size_center,
+                12,
+                'B'
+            );
+				$this->SetFontSize(round($this->FontSizePt,0,PHP_ROUND_HALF_DOWN));	
+            	$xid = $x0 + $this->wi / 2 - $this->GetStringWidth($this->abrev, $this->FontFamily, $this->FontStyle,$this->FontSizePt) / 2 + $this->wphoto/2 ;
+				}
+            $this->SetXY($xid , $y0 + 13);
+			$this->writeHTML('<strong>' . $this->abrev . '</strong>', true, false);
 
             // Name: Adapt font size to text length
             $this->SetTextColor(0);
@@ -208,43 +248,65 @@ class PdfMembersCardsAdaptative extends PdfMembersCards
                 8,
                 'B'
             );
-            if ($y0 + $this->hlogo + 3 < $y0 + 20) {
-                $this->SetXY($x0 + round($this->wi / 3.5) + 2, $y0 + 20);
+            if ($this->hlogo + 2 >= + 20) {
+                $this->SetXY($x0 + $this->wphoto + 2, $y0 + $this->hlogo + 2 + 1);
             } else {
-                $this->SetXY($x0 + round($this->wi / 3.5) + 2, $y0 + $this->hlogo + 1);
+                $this->SetXY($x0 + $this->wphoto + 2, $y0 + 20);
             }
-            //$this->setX($x0 + 27);
+            $this->SetFontSize(round($this->FontSizePt,0,PHP_ROUND_HALF_DOWN));
+			if ($this->FontSizePt <= 7) {
+				$this->SetFontSize(7);
+			$this->writeHTML('<strong>' . $member->stitle . " " . $member->name . '</strong>' , true, false);
+			$this->SetX($x0 + $this->wphoto + 2);
+			$this->writeHTML('<strong>' . $member->surname . '</strong>', true, false);
+				if ($this->he < 44) {
+				$this->ban_max_he = 10 ;
+				}
+			} else {
             $this->writeHTML('<strong>' . $nom_adh_ext . '</strong>', true, false);
+			}
 
             // Email (adapt too)
             $this->fixSize(
                 $email,
-                $this->max_text_size,
+                $this->max_text_size_center,
                 6,
                 'B'
             );
-            $this->setX($x0 + round($this->wi / 3.5) + 2);
+            $this->setX($x0 + $this->wphoto + 2);
             $this->writeHTML('<strong>' . $email . '</strong>', false, false);
 
             // Lower colored strip with long text
-
-            $nb_char =  round($this->wi / iconv_strlen($this->preferences->pref_card_strip) * 4.1);
-            if ($nb_char > 12) {
-                $nb_char = 12 ;
-            }
+            $this->SetFont(self::FONT,'B');
+			 $this->fixSize(
+                $this->preferences->pref_card_strip,
+                $this->max_text_size_full,
+                12,
+                'B'
+            );
+			if ($this->wi < 73) {
+				$this->SetFontSize(4.5);
+			}
+					
+			$this->SetFontSize(round($this->FontSizePt,0,PHP_ROUND_HALF_DOWN));
+    
             $this->SetFillColor($fcol['R'], $fcol['G'], $fcol['B']);
             $this->SetTextColor(
                 $this->tcol['R'],
                 $this->tcol['G'],
                 $this->tcol['B']
             );
-            $this->SetFont(self::FONT, 'B', $nb_char);
-            $this->SetXY($x0, $y0 + round($this->wi / 3.5) * 1.3 + 2);
+           $this->cell_he = ($this->he - round($this->hphoto)-2);
+			if ( $this->cell_he >=$this->ban_max_he) {
+				$this->cell_he = $this->ban_max_he ;
+			}
+			
+            $this->SetXY($x0, $y0 + $this->he - $this->cell_he);
             $this->Cell(
                 $this->wi,
-                ($this->he - (round($this->wi / 3.5) * 1.3 + 2)),
-                $this->preferences->pref_card_strip,
-                0,
+                $this->cell_he,
+                $this->preferences->pref_card_strip ,
+				0,
                 0,
                 'C',
                 true
